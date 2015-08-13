89ccfc868a87788cc34b56e1b6d93674aeef0913
Sahil Sharma <SahilDude89ss@gmail.com>
2015-08-13 10:39:29 +1000

    Adding dummy code index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ require_once('vendor/autoload.php');
 
 $clientBean = \Pyntax\PyntaxDAO::getBean('clients');
 $clientBean->users_id = 4;
-$clientBean->first_name = "Dhanoop";
-$clientBean->last_name = "Karunakarun";
-$clientBean->website = "dk.com";
+$clientBean->first_name = "Sahil";
+$clientBean->last_name = "Sharma";
+$clientBean->website = "pyntax.net";
 $clientBean->save();
 
 $clientBean->website = "http://hello.world.com";
